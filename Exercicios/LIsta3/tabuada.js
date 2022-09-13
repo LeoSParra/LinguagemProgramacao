b14b78db66584997717310c7129e27badc356b8e
LeoSParra <leonardoparra1k@gmail.com>
2022-09-13 09:14:10 -0300

    Commitando

--- Exercicios/LIsta3/tabuada.js
+++ Exercicios/LIsta3/tabuada.js
@@ -1,14 +1,13 @@
-function calcularTabuada(){
+function calcularTabuada() {
 
     let num = 1
 
-    while(num <= 10){
+    while (num <= 10) {
         let contador = 0
-        while(contador <= 10){
-            console.log(`${num} x ${contador} = ${num*contador}`)
+        while (contador <= 10) {
+            console.log(`${num} x ${contador} = ${num * contador}`)
             contador++
         }
         num++
-    }
-
+    }       
 }
